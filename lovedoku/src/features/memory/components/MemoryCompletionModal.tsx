25f55daf22d9805a6643832204f28c95eb832b8c
Akshit Mahaur <mahaurakshit7@gmail.com>
2025-09-07 18:45:39 +0530

    fix: Modal import path in MemoryCompletionModal
    - Corrected import path from ../../../../ to ../../../
    - Should resolve build error

--- lovedoku/src/features/memory/components/MemoryCompletionModal.tsx
+++ lovedoku/src/features/memory/components/MemoryCompletionModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Modal } from '../../../../shared/components/Modal';
+import { Modal } from '../../../shared/components/Modal';
 
 interface MemoryCompletionModalProps {
   isOpen: boolean;
